9945a7231bb8f13e2e2ed3578451d3786abdea24
Theo <tkmail2005@gmail.com>
2018-02-20 21:44:24 +0100

    Created footer

--- footer.php
+++ footer.php
@@ -1,3 +1,29 @@
 <?php
 ?>
-<div class="panel-footer">Эээээ(c)2017</div>
+<div class="panel-footer">
+	<div class="container-fluid">
+		<div class="row">
+			<div id="dark" class="offset-md-1 col-md-3">
+				<img src="img/twitter.png">
+				Tweet to us!
+			</div>
+			<div id="dark" class="col-md-2">
+				<img src="img/facebook.png">
+				Write us something!
+			</div>
+			<div id="dark" class="col-md-2">
+				<img src="img/google_plus.png">
+				Add us on G+!
+			</div>
+			<div id="dark" class="col-md-2">
+				<img src="img/rss.png">
+				Look us up on RSS!
+			</div>
+			<div id="dark" class="col-md-2">
+				<img src="img/discord.png">
+				Join our Discord!
+			</div>
+		</div>
+	</div>
+	<br>
+</div>
