8b8c3803f660d42a488e9fce944c593c4c7f6992
Theo <tkmail2005@gmail.com>
2018-02-22 12:20:31 +0100

    fixed some bugs

--- headerMenu.php
+++ headerMenu.php
@@ -1,54 +1,56 @@
-<header class="hidden-xs" id="unscroll">
-	<p id="whitebar">you shouldn't be able to see this</p>
-	<nav class="navbar navbar-default menu">
-		<div class="container">
-			<!-- Brand and toggle get grouped for better mobile display -->
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-				<span class="sr-only">Toggle navigation</span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-			</button>
-			</div>
-			<!-- Collect the nav links, forms, and other content for toggling -->
-			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-				<center>
-						<img src="img/logo03.png">
-				</center>
-				<ul class="nav navbar-nav">
-					<li class="active-link"><a href="#">item1</a></li>
-					<li><a href="#">item2</a></li>
-					<li><a href="#">item3</a></li>
-					<li><a href="#">item4</a></li>
-					<li><a href="#">item5</a></li>
-					<li><a href="#">item6</a></li>
-				</ul>
-			</div><!-- /.navbar-collapse -->
-		</div><!-- /.container-fluid -->
-	</nav>
+<header class="hidden-xs" id="unscroll"> 
+  <p id="whitebar">you shouldn't be able to see this</p> 
+  <nav class="navbar navbar-default menu"> 
+    <div class="container"> 
+      <!-- Brand and toggle get grouped for better mobile display --> 
+      <div class="navbar-header"> 
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false"> 
+        <span class="sr-only">Toggle navigation</span> 
+        <span class="icon-bar"></span> 
+        <span class="icon-bar"></span> 
+        <span class="icon-bar"></span> 
+      </button> 
+      </div> 
+      <!-- Collect the nav links, forms, and other content for toggling --> 
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1"> 
+        <center> 
+            <img src="img/logo03.png"> 
+        </center> 
+        <ul class="nav navbar-nav"> 
+          <li class="active-link"><a href="#">item1</a></li> 
+          <li><a href="#">item2</a></li> 
+          <li><a href="#">item3</a></li> 
+          <li><a href="#">item4</a></li> 
+          <li><a href="#">item5</a></li> 
+          <li><a href="#">item6</a></li> 
+        </ul> 
+      </div><!-- /.navbar-collapse --> 
+    </div><!-- /.container-fluid --> 
+  </nav> 
+</header> 
+ 
+ 
+<header class="hidden-sm hidden-md hidden-lg hidden-xl" id="unscroll"> 
+  <p id="whitebar">you shouldn't be able to see this</p> 
+    <nav class="navbar navbar-default menu"> 
+      <div class="container"> 
+        <!-- Brand and toggle get grouped for better mobile display --> 
+        <div class="navbar-header"> 
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false"> 
+            <span class="sr-only">Toggle navigation</span> 
+            <span class="icon-bar"></span> 
+            <span class="icon-bar"></span> 
+            <span class="icon-bar"></span> 
+          </button> 
+        </div> 
+        <!-- Collect the nav links, forms, and other content for toggling --> 
+      <div class="" id="bs-example-navbar-collapse-1"> 
+        <center> 
+          <img src="img/logo03.png"> 
+        </center> 
+      </div><!-- /.navbar-collapse --> 
+    </div><!-- /.container-fluid --> 
+  </nav> 
 </header>
-
-
-<header class="hidden-sm hidden-md hidden-lg hidden-xl" id="unscroll">
-	<p id="whitebar">you shouldn't be able to see this</p>
-		<nav class="navbar navbar-default menu">
-			<div class="container">
-				<!-- Brand and toggle get grouped for better mobile display -->
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-						<span class="sr-only">Toggle navigation</span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-					</button>
-				</div>
-				<!-- Collect the nav links, forms, and other content for toggling -->
-			<div class="" id="bs-example-navbar-collapse-1">
-				<center>
-					<img src="img/logo03.png">
-				</center>
-			</div><!-- /.navbar-collapse -->
-		</div><!-- /.container-fluid -->
-	</nav>
-</header>
+<br><br><br><br><br>
+<br><br><br><br><br>
