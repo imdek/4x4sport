ce5f18257708a40aa5e14bf6faefaa3bdbab3289
Theo <tkmail2005@gmail.com>
2018-03-02 21:43:48 +0100

    Added sort testing

--- sorting.php
+++ sorting.php
@@ -10,6 +10,25 @@
 		<h4><input type="checkbox" name="Trophy" value="trophy"> Trophy</h4>
 		<h4><input type="checkbox" name="Trial" value="trial"> Trial</h4>
 		<h4><input type="checkbox" name="Meeting" value="meeting"> Meeting</h4>
-		<button id="sortbutton">Find</button>
+		<button onClick="find()" id="sortbutton">Find</button>
 	</center>
 </div>
+<script>
+	function find() {
+		var text="We will now only show you events with the tags ";
+		if(document.querySelector('input[name=Rally]').checked){
+			text+="\'Rally\' "
+		}
+		if(document.querySelector('input[name=Trophy]').checked){
+			text+="\'Trophy\' "
+		}
+		if(document.querySelector('input[name=Trial]').checked){
+			text+="\'Trial\' "
+		}
+		if(document.querySelector('input[name=Meeting]').checked){
+			text+="\'Meeting\'"
+		}
+		text+=".";
+		alert(text);
+	}
+</script>
