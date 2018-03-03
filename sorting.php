614a799d96e8369d66b0268253d45846f0cdd58c
Theo <tkmail2005@gmail.com>
2018-03-03 11:33:56 +0100

    Changed the design a little

--- sorting.php
+++ sorting.php
@@ -1,17 +1,17 @@
 <link href="sheet.css" rel="stylesheet">
 
 <div id="sorting">
-	<br>
 	<center>
 		<img src="img/sorting.png">
-		<h2>Sorting</h2>
+		<h3>Sorting</h3>
 		I want to see:<br>
-		<h4><input type="checkbox" name="Rally" value="rally"> Rally</h4>
-		<h4><input type="checkbox" name="Trophy" value="trophy"> Trophy</h4>
-		<h4><input type="checkbox" name="Trial" value="trial"> Trial</h4>
-		<h4><input type="checkbox" name="Meeting" value="meeting"> Meeting</h4>
-		<button id="sortbutton">Find</button>
 	</center>
+	<h4><input type="checkbox" name="Rally" value="rally"> Rally</h4>
+	<h4><input type="checkbox" name="Trophy" value="trophy"> Trophy</h4>
+	<h4><input type="checkbox" name="Trial" value="trial"> Trial</h4>
+	<h4><input type="checkbox" name="Meeting" value="meeting"> Meeting</h4>
+	<button id="sortbutton">Find</button>
+	
 </div>
 <script>
 	function find (){
